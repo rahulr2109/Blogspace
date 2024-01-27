f5b7854337175371da617fed4170133ac1e22016
Rahul Rai <rahul0921r@gmail.com>
2024-01-27 20:18:06 +0530

    cors added

--- server/server.js
+++ server/server.js
@@ -24,7 +24,13 @@ let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; // regex for e
 let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/; // regex for password
 
 server.use(express.json());
-server.use(cors());
+server.use(
+  cors({
+    origin: ["https://blogging-web-app-frontend.vercel.app/"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 mongoose.connect(process.env.DB_LOCATION, {
   autoIndex: true,
